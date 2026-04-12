1b04750d236000ecbd0e19498ca303c199ba331e
mIKLER <miklerperez@gmail.com>
2026-04-12 12:10:59 -0500

    Update Maleta.java

--- src/main/java/com/tasfb2b/planificador/model/Maleta.java
+++ src/main/java/com/tasfb2b/planificador/model/Maleta.java
@@ -1,5 +1,8 @@
 package com.tasfb2b.planificador.model;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -7,6 +10,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -22,7 +26,17 @@ public class Maleta {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @NotBlank
+    @Column(nullable = false)
+    private LocalDateTime fechaHoraRegistro;
+
+    @NotBlank
+    @Column(nullable = false)
+    private Integer plazo;
+
     @ManyToOne
     @JoinColumn(name = "cliente_id", nullable = false)
     private Cliente cliente;
+
+
 }
