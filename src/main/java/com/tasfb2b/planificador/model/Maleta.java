9bde48eb5f5e77fc60adad2c3a5b7fb30072ad46
Sandy <a20201230@pucp.edu.pe>
2026-04-22 10:20:08 -0500

    Adaptación de objeto maleta

--- src/main/java/com/tasfb2b/planificador/model/Maleta.java
+++ src/main/java/com/tasfb2b/planificador/model/Maleta.java
@@ -11,6 +11,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -26,11 +27,11 @@ public class Maleta {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @NotBlank
+    @NotNull
     @Column(nullable = false)
     private LocalDateTime fechaHoraRegistro;
 
-    @NotBlank
+    @NotNull
     @Column(nullable = false)
     private Integer plazo;
 
@@ -38,5 +39,19 @@ public class Maleta {
     @JoinColumn(name = "cliente_id", nullable = false)
     private Cliente cliente;
 
+    @ManyToOne
+    @JoinColumn(name = "aeropuerto_origen_id", nullable = false)
+    private Aeropuerto aeropuertoOrigen;
 
+    @ManyToOne
+    @JoinColumn(name = "aeropuerto_destino_id", nullable = false)
+    private Aeropuerto aeropuertoDestino;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String idEnvio;
+
+    @NotNull
+    @Column(nullable = false)
+    private Integer cantidad;
 }
