3b9be0b05a593955072cc0a46f8ebbc83082b4d6
Alfredo123255 <a20220825@pucp.edu.pe>
2026-05-28 20:18:53 -0500

    Sin migracion

--- src/main/java/com/tasfb2b/planificador/PlanificadorApplication.java
+++ src/main/java/com/tasfb2b/planificador/PlanificadorApplication.java
@@ -39,15 +39,15 @@ public class PlanificadorApplication {
     }
 
 
-    @Bean
+    /*@Bean
     CommandLineRunner initDatabase(MigradorEnviosDb migrador) {
         return args -> {
             // Pon aquí la ruta absoluta de tu computadora
             String ruta = "E:\\Codes\\GIT\\Backend-Planificador-TASFB2B\\data\\_envios_preliminar_";
-            
+
             System.out.println("Iniciando migración masiva a PostgreSQL...");
             // Descomenta la línea de abajo SOLO CUANDO QUIERAS LLENAR LA BASE DE DATOS
             migrador.migrarDirectorioCompleto(ruta);
         };
-    }
+    }*/
 }
